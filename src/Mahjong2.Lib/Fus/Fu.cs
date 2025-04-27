e8aeb0e52aab3e0f217727e8984ef47ef8cb6062
kitada <taihei.kitada@gmail.com>
2025-04-27 17:59:07 +0900

    符追加

---
+++ src/Mahjong2.Lib/Fus/Fu.cs
@@ -0,0 +1,353 @@
+﻿namespace Mahjong2.Lib.Fus;
+
+/// <summary>
+/// 符を表現するクラス
+/// </summary>
+public abstract record Fu
+{
+    #region シングルトンプロパティ
+
+    /// <summary>
+    /// 副底
+    /// </summary>
+    public static Futei Futei { get; } = new();
+    /// <summary>
+    /// 順子
+    /// </summary>
+    public static Shuntsu Shuntsu { get; } = new();
+    /// <summary>
+    /// 中張牌の明刻
+    /// </summary>
+    public static MinkoChuchan MinkoChuchan { get; } = new();
+    /// <summary>
+    /// 么九牌の明刻
+    /// </summary>
+    public static MinkoYaochu MinkoYaochu { get; } = new();
+    /// <summary>
+    /// 中張牌の暗刻
+    /// </summary>
+    public static AnkoChuchan AnkoChuchan { get; } = new();
+    /// <summary>
+    /// 么九牌の暗刻
+    /// </summary>
+    public static AnkoYaochu AnkoYaochu { get; } = new();
+    /// <summary>
+    /// 中張牌の明槓
+    /// </summary>
+    public static MinkanChuchan MinkanChuchan { get; } = new();
+    /// <summary>
+    /// 么九牌の明槓
+    /// </summary>
+    public static MinkanYaochu MinkanYaochu { get; } = new();
+    /// <summary>
+    /// 中張牌の暗槓
+    /// </summary>
+    public static AnkanChuchan AnkanChuchan { get; } = new();
+    /// <summary>
+    /// 么九牌の暗槓
+    /// </summary>
+    public static AnkanYaochu AnkanYaochu { get; } = new();
+    /// <summary>
+    /// 数牌の雀頭
+    /// </summary>
+    public static JantoNumber JantoNumber { get; } = new();
+    /// <summary>
+    /// 客風の雀頭
+    /// </summary>
+    public static JantoOtherWind JantoOtherWind { get; } = new();
+    /// <summary>
+    /// 自風の雀頭
+    /// </summary>
+    public static JantouPlayerWind JantouPlayerWind { get; } = new();
+    /// <summary>
+    /// 場風の雀頭
+    /// </summary>
+    public static JantouRoundWind JantouRoundWind { get; } = new();
+    /// <summary>
+    /// 三元牌の雀頭
+    /// </summary>
+    public static JantouDragon JantouDragon { get; } = new();
+    /// <summary>
+    /// 両面待ち
+    /// </summary>
+    public static WaitRyanmen WaitRyanmen { get; } = new();
+    /// <summary>
+    /// シャンポン待ち
+    /// </summary>
+    public static WaitShanpon WaitShanpon { get; } = new();
+    /// <summary>
+    /// カンチャン待ち
+    /// </summary>
+    public static WaitKanchan WaitKanchan { get; } = new();
+    /// <summary>
+    /// ペンチャン待ち
+    /// </summary>
+    public static WaitPenchan WaitPenchan { get; } = new();
+    /// <summary>
+    /// 単騎待ち
+    /// </summary>
+    public static WaitTanki WaitTanki { get; } = new();
+    /// <summary>
+    /// 門前加符
+    /// </summary>
+    public static MenzenFu MenzenFu { get; } = new();
+    /// <summary>
+    /// ツモ符
+    /// </summary>
+    public static TsumoFu TsumoFu { get; } = new();
+
+    #endregion シングルトンプロパティ
+
+    /// <summary>
+    /// 名前
+    /// </summary>
+    public abstract string Name { get; }
+
+    /// <summary>
+    /// 値
+    /// </summary>
+    public abstract int Value { get; }
+
+    public override string ToString()
+    {
+        return $"{Name}:{Value}符";
+    }
+}
+
+/// <summary>
+/// 副底
+/// </summary>
+public record Futei : Fu
+{
+    public override string Name { get; } = "副底";
+    public override int Value { get; } = 20;
+}
+
+/// <summary>
+/// 面子
+/// </summary>
+public abstract record Mentsu : Fu;
+
+/// <summary>
+/// 順子
+/// </summary>
+public record Shuntsu : Mentsu
+{
+    public override string Name { get; } = "順子";
+    public override int Value { get; } = 0;
+}
+
+/// <summary>
+/// 刻子
+/// </summary>
+public abstract record Koutsu : Mentsu;
+
+/// <summary>
+/// 明刻
+/// </summary>
+public abstract record Minko : Koutsu;
+
+/// <summary>
+/// 中張牌の明刻
+/// </summary>
+public record MinkoChuchan : Minko
+{
+    public override string Name { get; } = "中張牌の明刻";
+    public override int Value { get; } = 2;
+}
+
+/// <summary>
+/// 么九牌の明刻
+/// </summary>
+public record MinkoYaochu : Minko
+{
+    public override string Name { get; } = "么九牌の明刻";
+    public override int Value { get; } = 4;
+}
+
+/// <summary>
+/// 暗刻
+/// </summary>
+public abstract record Anko : Koutsu;
+
+/// <summary>
+/// 中張牌の暗刻
+/// </summary>
+public record AnkoChuchan : Anko
+{
+    public override string Name { get; } = "中張牌の暗刻";
+    public override int Value { get; } = 4;
+}
+
+/// <summary>
+/// 么九牌の暗刻
+/// </summary>
+public record AnkoYaochu : Anko
+{
+    public override string Name { get; } = "么九牌の暗刻";
+    public override int Value { get; } = 8;
+}
+
+/// <summary>
+/// 明槓
+/// </summary>
+public abstract record Minkan : Koutsu;
+
+/// <summary>
+/// 中張牌の明槓
+/// </summary>
+public record MinkanChuchan : Minkan
+{
+    public override string Name { get; } = "中張牌の明槓";
+    public override int Value { get; } = 8;
+}
+
+/// <summary>
+/// 么九牌の明槓
+/// </summary>
+public record MinkanYaochu : Minkan
+{
+    public override string Name { get; } = "么九牌の明槓";
+    public override int Value { get; } = 16;
+}
+
+/// <summary>
+/// 暗槓
+/// </summary>
+public abstract record Ankan : Koutsu;
+
+/// <summary>
+/// 中張牌の暗槓
+/// </summary>
+public record AnkanChuchan : Ankan
+{
+    public override string Name { get; } = "中張牌の暗槓";
+    public override int Value { get; } = 16;
+}
+
+/// <summary>
+/// 么九牌の暗槓
+/// </summary>
+public record AnkanYaochu : Ankan
+{
+    public override string Name { get; } = "么九牌の暗槓";
+    public override int Value { get; } = 32;
+}
+
+/// <summary>
+/// 雀頭
+/// </summary>
+public abstract record Jantou : Fu;
+
+/// <summary>
+/// 数牌の雀頭
+/// </summary>
+public record JantoNumber : Jantou
+{
+    public override string Name { get; } = "数牌の雀頭";
+    public override int Value { get; } = 0;
+}
+
+/// <summary>
+/// 客風の雀頭
+/// </summary>
+public record JantoOtherWind : Jantou
+{
+    public override string Name { get; } = "客風の雀頭";
+    public override int Value { get; } = 0;
+}
+
+/// <summary>
+/// 自風の雀頭
+/// </summary>
+public record JantouPlayerWind : Jantou
+{
+    public override string Name { get; } = "自風の雀頭";
+    public override int Value { get; } = 2;
+}
+
+/// <summary>
+/// 場風の雀頭
+/// </summary>
+public record JantouRoundWind : Jantou
+{
+    public override string Name { get; } = "場風の雀頭";
+    public override int Value { get; } = 2;
+}
+
+/// <summary>
+/// 三元牌の雀頭
+/// </summary>
+public record JantouDragon : Jantou
+{
+    public override string Name { get; } = "三元牌の雀頭";
+    public override int Value { get; } = 2;
+}
+
+/// <summary>
+/// 待ち
+/// </summary>
+public abstract record Wait : Fu;
+
+/// <summary>
+/// 両面待ち
+/// </summary>
+public record WaitRyanmen : Wait
+{
+    public override string Name { get; } = "両面待ち";
+    public override int Value { get; } = 0;
+}
+
+/// <summary>
+/// シャンポン待ち
+/// </summary>
+public record WaitShanpon : Wait
+{
+    public override string Name { get; } = "シャンポン待ち";
+    public override int Value { get; } = 0;
+}
+
+/// <summary>
+/// カンチャン待ち
+/// </summary>
+public record WaitKanchan : Wait
+{
+    public override string Name { get; } = "カンチャン待ち";
+    public override int Value { get; } = 2;
+}
+
+/// <summary>
+/// ペンチャン待ち
+/// </summary>
+public record WaitPenchan : Wait
+{
+    public override string Name { get; } = "ペンチャン待ち";
+    public override int Value { get; } = 2;
+}
+
+/// <summary>
+/// 単騎待ち
+/// </summary>
+public record WaitTanki : Wait
+{
+    public override string Name { get; } = "単騎待ち";
+    public override int Value { get; } = 2;
+}
+
+/// <summary>
+/// 門前加符
+/// </summary>
+public record MenzenFu : Fu
+{
+    public override string Name { get; } = "門前加符";
+    public override int Value { get; } = 10;
+}
+
+/// <summary>
+/// ツモ符
+/// </summary>
+public record TsumoFu : Fu
+{
+    public override string Name { get; } = "ツモ符";
+    public override int Value { get; } = 2;
+}
---
+++ src/Mahjong2.Tests/Fus/FuTests.cs
@@ -0,0 +1,416 @@
+using Mahjong2.Lib.Fus;
+using Xunit;
+
+namespace Mahjong2.Tests.Fus
+{
+    /// <summary>
+    /// Fuクラスとその派生クラスのテスト
+    /// </summary>
+    public class FuTests
+    {
+        /// <summary>
+        /// Futei_値取得_20が返る
+        /// </summary>
+        [Fact]
+        public void Futei_値取得_20が返る()
+        {
+            // Arrange
+            var futei = Fu.Futei;
+
+            // Act
+            var value = futei.Value;
+
+            // Assert
+            Assert.Equal(20, value);
+        }
+
+        /// <summary>
+        /// Shuntsu_値取得_0が返る
+        /// </summary>
+        [Fact]
+        public void Shuntsu_値取得_0が返る()
+        {
+            // Arrange
+            var shuntsu = Fu.Shuntsu;
+
+            // Act
+            var value = shuntsu.Value;
+
+            // Assert
+            Assert.Equal(0, value);
+        }
+
+        /// <summary>
+        /// MinkoChuchan_値取得_2が返る
+        /// </summary>
+        [Fact]
+        public void MinkoChuchan_値取得_2が返る()
+        {
+            // Arrange
+            var minkoChuchan = Fu.MinkoChuchan;
+
+            // Act
+            var value = minkoChuchan.Value;
+
+            // Assert
+            Assert.Equal(2, value);
+        }
+
+        /// <summary>
+        /// MinkoYaochu_値取得_4が返る
+        /// </summary>
+        [Fact]
+        public void MinkoYaochu_値取得_4が返る()
+        {
+            // Arrange
+            var minkoYaochu = Fu.MinkoYaochu;
+
+            // Act
+            var value = minkoYaochu.Value;
+
+            // Assert
+            Assert.Equal(4, value);
+        }
+
+        /// <summary>
+        /// AnkoChuchan_値取得_4が返る
+        /// </summary>
+        [Fact]
+        public void AnkoChuchan_値取得_4が返る()
+        {
+            // Arrange
+            var ankoChuchan = Fu.AnkoChuchan;
+
+            // Act
+            var value = ankoChuchan.Value;
+
+            // Assert
+            Assert.Equal(4, value);
+        }
+
+        /// <summary>
+        /// AnkoYaochu_値取得_8が返る
+        /// </summary>
+        [Fact]
+        public void AnkoYaochu_値取得_8が返る()
+        {
+            // Arrange
+            var ankoYaochu = Fu.AnkoYaochu;
+
+            // Act
+            var value = ankoYaochu.Value;
+
+            // Assert
+            Assert.Equal(8, value);
+        }
+
+        /// <summary>
+        /// MinkanChuchan_値取得_8が返る
+        /// </summary>
+        [Fact]
+        public void MinkanChuchan_値取得_8が返る()
+        {
+            // Arrange
+            var minkanChuchan = Fu.MinkanChuchan;
+
+            // Act
+            var value = minkanChuchan.Value;
+
+            // Assert
+            Assert.Equal(8, value);
+        }
+
+        /// <summary>
+        /// MinkanYaochu_値取得_16が返る
+        /// </summary>
+        [Fact]
+        public void MinkanYaochu_値取得_16が返る()
+        {
+            // Arrange
+            var minkanYaochu = Fu.MinkanYaochu;
+
+            // Act
+            var value = minkanYaochu.Value;
+
+            // Assert
+            Assert.Equal(16, value);
+        }
+
+        /// <summary>
+        /// AnkanChuchan_値取得_16が返る
+        /// </summary>
+        [Fact]
+        public void AnkanChuchan_値取得_16が返る()
+        {
+            // Arrange
+            var ankanChuchan = Fu.AnkanChuchan;
+
+            // Act
+            var value = ankanChuchan.Value;
+
+            // Assert
+            Assert.Equal(16, value);
+        }
+
+        /// <summary>
+        /// AnkanYaochu_値取得_32が返る
+        /// </summary>
+        [Fact]
+        public void AnkanYaochu_値取得_32が返る()
+        {
+            // Arrange
+            var ankanYaochu = Fu.AnkanYaochu;
+
+            // Act
+            var value = ankanYaochu.Value;
+
+            // Assert
+            Assert.Equal(32, value);
+        }
+
+        /// <summary>
+        /// JantoNumber_値取得_0が返る
+        /// </summary>
+        [Fact]
+        public void JantoNumber_値取得_0が返る()
+        {
+            // Arrange
+            var jantoNumber = Fu.JantoNumber;
+
+            // Act
+            var value = jantoNumber.Value;
+
+            // Assert
+            Assert.Equal(0, value);
+        }
+
+        /// <summary>
+        /// JantoOtherWind_値取得_0が返る
+        /// </summary>
+        [Fact]
+        public void JantoOtherWind_値取得_0が返る()
+        {
+            // Arrange
+            var jantoOtherWind = Fu.JantoOtherWind;
+
+            // Act
+            var value = jantoOtherWind.Value;
+
+            // Assert
+            Assert.Equal(0, value);
+        }
+
+        /// <summary>
+        /// JantouPlayerWind_値取得_2が返る
+        /// </summary>
+        [Fact]
+        public void JantouPlayerWind_値取得_2が返る()
+        {
+            // Arrange
+            var jantouPlayerWind = Fu.JantouPlayerWind;
+
+            // Act
+            var value = jantouPlayerWind.Value;
+
+            // Assert
+            Assert.Equal(2, value);
+        }
+
+        /// <summary>
+        /// JantouRoundWind_値取得_2が返る
+        /// </summary>
+        [Fact]
+        public void JantouRoundWind_値取得_2が返る()
+        {
+            // Arrange
+            var jantouRoundWind = Fu.JantouRoundWind;
+
+            // Act
+            var value = jantouRoundWind.Value;
+
+            // Assert
+            Assert.Equal(2, value);
+        }
+
+        /// <summary>
+        /// JantouDragon_値取得_2が返る
+        /// </summary>
+        [Fact]
+        public void JantouDragon_値取得_2が返る()
+        {
+            // Arrange
+            var jantouDragon = Fu.JantouDragon;
+
+            // Act
+            var value = jantouDragon.Value;
+
+            // Assert
+            Assert.Equal(2, value);
+        }
+
+        /// <summary>
+        /// WaitRyanmen_値取得_0が返る
+        /// </summary>
+        [Fact]
+        public void WaitRyanmen_値取得_0が返る()
+        {
+            // Arrange
+            var waitRyanmen = Fu.WaitRyanmen;
+
+            // Act
+            var value = waitRyanmen.Value;
+
+            // Assert
+            Assert.Equal(0, value);
+        }
+
+        /// <summary>
+        /// WaitShanpon_値取得_0が返る
+        /// </summary>
+        [Fact]
+        public void WaitShanpon_値取得_0が返る()
+        {
+            // Arrange
+            var waitShanpon = Fu.WaitShanpon;
+
+            // Act
+            var value = waitShanpon.Value;
+
+            // Assert
+            Assert.Equal(0, value);
+        }
+
+        /// <summary>
+        /// WaitKanchan_値取得_2が返る
+        /// </summary>
+        [Fact]
+        public void WaitKanchan_値取得_2が返る()
+        {
+            // Arrange
+            var waitKanchan = Fu.WaitKanchan;
+
+            // Act
+            var value = waitKanchan.Value;
+
+            // Assert
+            Assert.Equal(2, value);
+        }
+
+        /// <summary>
+        /// WaitPenchan_値取得_2が返る
+        /// </summary>
+        [Fact]
+        public void WaitPenchan_値取得_2が返る()
+        {
+            // Arrange
+            var waitPenchan = Fu.WaitPenchan;
+
+            // Act
+            var value = waitPenchan.Value;
+
+            // Assert
+            Assert.Equal(2, value);
+        }
+
+        /// <summary>
+        /// WaitTanki_値取得_2が返る
+        /// </summary>
+        [Fact]
+        public void WaitTanki_値取得_2が返る()
+        {
+            // Arrange
+            var waitTanki = Fu.WaitTanki;
+
+            // Act
+            var value = waitTanki.Value;
+
+            // Assert
+            Assert.Equal(2, value);
+        }
+
+        /// <summary>
+        /// MenzenFu_値取得_10が返る
+        /// </summary>
+        [Fact]
+        public void MenzenFu_値取得_10が返る()
+        {
+            // Arrange
+            var menzenFu = Fu.MenzenFu;
+
+            // Act
+            var value = menzenFu.Value;
+
+            // Assert
+            Assert.Equal(10, value);
+        }
+
+        /// <summary>
+        /// TsumoFu_値取得_2が返る
+        /// </summary>
+        [Fact]
+        public void TsumoFu_値取得_2が返る()
+        {
+            // Arrange
+            var tsumoFu = Fu.TsumoFu;
+
+            // Act
+            var value = tsumoFu.Value;
+
+            // Assert
+            Assert.Equal(2, value);
+        }
+
+        /// <summary>
+        /// シングルトンの一意性をテストする
+        /// </summary>
+        [Fact]
+        public void Fu_シングルトンインスタンス取得_同一インスタンスが返る()
+        {
+            // Arrange & Act
+            var futei1 = Fu.Futei;
+            var futei2 = Fu.Futei;
+            
+            var shuntsu1 = Fu.Shuntsu;
+            var shuntsu2 = Fu.Shuntsu;
+
+            // Assert
+            Assert.Same(futei1, futei2);
+            Assert.Same(shuntsu1, shuntsu2);
+        }
+
+        /// <summary>
+        /// 継承関係の検証
+        /// </summary>
+        [Fact]
+        public void Fu_継承関係の検証_正しい型に変換できる()
+        {
+            // Arrange & Act & Assert
+            Assert.IsType<Futei>(Fu.Futei);
+            Assert.IsType<Shuntsu>(Fu.Shuntsu);
+            
+            Assert.IsType<MinkoChuchan>(Fu.MinkoChuchan);
+            Assert.IsType<MinkoYaochu>(Fu.MinkoYaochu);
+            Assert.IsType<AnkoChuchan>(Fu.AnkoChuchan);
+            Assert.IsType<AnkoYaochu>(Fu.AnkoYaochu);
+            Assert.IsType<MinkanChuchan>(Fu.MinkanChuchan);
+            Assert.IsType<MinkanYaochu>(Fu.MinkanYaochu);
+            Assert.IsType<AnkanChuchan>(Fu.AnkanChuchan);
+            Assert.IsType<AnkanYaochu>(Fu.AnkanYaochu);
+            
+            Assert.IsType<JantoNumber>(Fu.JantoNumber);
+            Assert.IsType<JantoOtherWind>(Fu.JantoOtherWind);
+            Assert.IsType<JantouPlayerWind>(Fu.JantouPlayerWind);
+            Assert.IsType<JantouRoundWind>(Fu.JantouRoundWind);
+            Assert.IsType<JantouDragon>(Fu.JantouDragon);
+            
+            Assert.IsType<WaitRyanmen>(Fu.WaitRyanmen);
+            Assert.IsType<WaitShanpon>(Fu.WaitShanpon);
+            Assert.IsType<WaitKanchan>(Fu.WaitKanchan);
+            Assert.IsType<WaitPenchan>(Fu.WaitPenchan);
+            Assert.IsType<WaitTanki>(Fu.WaitTanki);
+            
+            Assert.IsType<MenzenFu>(Fu.MenzenFu);
+            Assert.IsType<TsumoFu>(Fu.TsumoFu);
+        }
+    }
+}
