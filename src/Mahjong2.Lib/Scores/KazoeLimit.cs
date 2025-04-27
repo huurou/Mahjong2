f86f80b9694f774864244c9a9b48c608a9b10dd4
kitada <taihei.kitada@gmail.com>
2025-04-28 01:33:10 +0900

    点数計算に関わるルール系を追加

---
+++ src/Mahjong2.Lib/Scores/KazoeLimit.cs
@@ -0,0 +1,20 @@
+﻿namespace Mahjong2.Lib.Scores;
+
+/// <summary>
+/// 数え役満のルール
+/// </summary>
+public enum KazoeLimit
+{
+    /// <summary>
+    /// 13翻以上は全て数え役満
+    /// </summary>
+    Limited = 0,
+    /// <summary>
+    /// 13翻以上は全て三倍満
+    /// </summary>
+    Sanbaiman = 1,
+    /// <summary>
+    /// 13翻以上は13翻ごとに数え役満が重なる
+    /// </summary>
+    Nolimit = 2
+}
---
+++ src/Mahjong2.Lib/Scores/WinSituation.cs
@@ -0,0 +1,82 @@
+﻿namespace Mahjong2.Lib.Scores;
+
+/// <summary>
+/// 和了時の状況
+/// </summary>
+public record WinSituation
+{
+    /// <summary>
+    /// ツモアガリかどうか
+    /// </summary>
+    public bool IsTsumo { get; init; } = false;
+
+    /// <summary>
+    /// リーチしているかどうか
+    /// </summary>
+    public bool IsRiichi { get; init; } = false;
+
+    /// <summary>
+    /// 一発かどうか
+    /// </summary>
+    public bool IsIppatsu { get; init; } = false;
+
+    /// <summary>
+    /// 嶺上開花かどうか
+    /// </summary>
+    public bool IsRinchan { get; init; } = false;
+
+    /// <summary>
+    /// 槍槓かどうか
+    /// </summary>
+    public bool IsChankan { get; init; } = false;
+
+    /// <summary>
+    /// 海底撈月かどうか
+    /// </summary>
+    public bool IsHaitei { get; init; } = false;
+
+    /// <summary>
+    /// 河底撈魚かどうか
+    /// </summary>
+    public bool IsHoutei { get; init; } = false;
+
+    /// <summary>
+    /// ダブルリーチかどうか
+    /// </summary>
+    public bool IsDoubleRiichi { get; init; } = false;
+
+    /// <summary>
+    /// 流し満貫かどうか
+    /// </summary>
+    public bool IsNagashiMangan { get; init; } = false;
+
+    /// <summary>
+    /// 天和かどうか
+    /// </summary>
+    public bool IsTenhou { get; init; } = false;
+
+    /// <summary>
+    /// 地和かどうか
+    /// </summary>
+    public bool IsChiihou { get; init; } = false;
+
+    /// <summary>
+    /// 人和かどうか
+    /// </summary>
+    public bool IsRenhou { get; init; } = false;
+
+    /// <summary>
+    /// 和了者の風
+    /// </summary>
+    public Wind PlayerWind { get; init; } = Wind.East;
+
+    /// <summary>
+    /// 場風
+    /// </summary>
+    public Wind RoundWind { get; init; } = Wind.East;
+
+    /// <summary>
+    /// プレイヤーが親かどうか
+    /// </summary>
+    public bool IsDealer => PlayerWind == Wind.East;
+}
